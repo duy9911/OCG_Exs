14e105a2dcf9d883edfc09ca8c1f0a4474dc15f5
duy9911 <ngoduy9911@gmail.com>
2021-09-09 10:44:26 +0700

    add 7

---
+++ OCG_Exs/lec-07/self-study/calulator/src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button class="button" @click="handlerUserTyping">{{ text }}</button>
+</template>
+<script>
+export default {
+  name: "Button",
+  props: { text: String },
+  data() {
+    return {
+      theFirstInput: true,
+    };
+  },
+  computed: {
+    handlerUserTyping() {
+      console.log(this.text);
+      switch (this.text) {
+        case "C":
+        case "CE":
+          return this.callClearScreen;
+        case "=":
+          return this.callDoMath;
+        default:
+          return this.callAddToScreen;
+      }
+    },
+  },
+  // actice event call respective func (active event goi den func tuong ung)
+  methods: {
+    callAddToScreen() {
+      this.$emit("add-screen", this.text);
+    },
+    callDoMath() {
+      this.$emit("do-math");
+    },
+    callClearScreen() {
+      this.$emit("clear-screen");
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
